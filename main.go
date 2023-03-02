3d9183c55a9f3223b4ac8a71eb5708b09c418beb
Will Gleason <wgleason9@gmail.com>
2023-03-02 11:06:01 -0800

    slow progress, add gin, build skeleton, add types

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/url"
 	"strconv"
 )
 
 type SearchArgs struct {
-	year          int
-	month         int
-	day           int
-	end_year      int
-	end_month     int
 	terms         []string
 	codes         []string
 	cursor        int
@@ -20,6 +16,9 @@ type SearchArgs struct {
 	link_term     string
 	link_distance int
 	source        string
+	sort          string
+	start_date    string
+	end_date      string
 }
 
 func cast_query_args_to_int(query_args url.Values, key string) (int, error) {
@@ -31,14 +30,23 @@ func cast_query_args_to_int(query_args url.Values, key string) (int, error) {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := gin.Default()
+	r.GET("/search", func(c *gin.Context) {
 		var query SearchArgs
-		request_args := r.URL.Query()
-		query.year, _ = cast_query_args_to_int(request_args, "year")
-		query.month, _ = cast_query_args_to_int(request_args, "month")
-		query.day, _ = cast_query_args_to_int(request_args, "day")
-		query.end_year, _ = cast_query_args_to_int(request_args, "end_year")
-		query.end_month, _ = cast_query_args_to_int(request_args, "end_month")
+		get_date_string := func(year int, month int) string {
+			if month != 0 {
+				return fmt.Sprintf("%d-%d", year, month)
+			} else {
+				return fmt.Sprintf("%d", year)
+			}
+		}
+		request_args := c.Request.URL.Query()
+		year, _ := cast_query_args_to_int(request_args, "year")
+		month, _ := cast_query_args_to_int(request_args, "month")
+		end_year, _ := cast_query_args_to_int(request_args, "end_year")
+		end_month, _ := cast_query_args_to_int(request_args, "end_month")
+		query.start_date = get_date_string(year, month)
+		query.end_date = get_date_string(end_year, end_month)
 		query.cursor, _ = cast_query_args_to_int(request_args, "cursor")
 		query.limit, _ = cast_query_args_to_int(request_args, "limit")
 		query.link_distance, _ = cast_query_args_to_int(request_args, "link_distance")
@@ -46,21 +54,41 @@ func main() {
 		query.codes = request_args["codes"]
 		query.link_term = request_args.Get("link_term")
 		query.source = request_args.Get("source")
-		fmt.Println(query)
-		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+		query.sort = request_args.Get("sort")
+		c.JSON(http.StatusOK, gin.H{"start": start, "end": end})
 	})
-
-	http.ListenAndServe(":8080", nil)
+	r.Run()
 }
 
 func get_row_context(args SearchArgs) {
-	total_records := 0
-	original_url := ""
+}
+
+func rest_args_to_gallica_params(args SearchArgs) GallicaQueryParams {
+	cql := "make string"
+	
+	return GallicaQueryParams{
+		operation:      "searchRetrieve",
+		exactSearch:    true,
+		version:        1.2,
+		startRecord:    args.cursor,
+		maximumRecords: args.limit,
+		query:          cql,
+		collapsing:     false,
+	}
 
 }
 
+type GallicaQueryParams struct {
+	operation      string
+	exactSearch    bool
+	version        float32
+	startRecord    int
+	maximumRecords int
+	query          string
+	collapsing     bool
+}
+
 type GallicaRecord struct {
-	data string
 }
 
 type GallicaWrapper interface {
